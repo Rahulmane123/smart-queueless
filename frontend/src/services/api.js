fcceb7901c0e71d0a8d9bf58449d225b33562658
rahul mane <rm5583857@gmail.com>
2026-03-19 02:25:09 +0530

    fix wrong api route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-queueless-kz4a.onrender.com/api/auth",
+  baseURL: "https://smart-queueless-kz4a.onrender.com/api",
 });
 
 // 🔐 Attach token to every request
